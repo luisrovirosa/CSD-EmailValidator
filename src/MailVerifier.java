eb50b7ea6ef2f17cf7d3a35d20e4a015cdbdaca2
Luis <luisrovirosa@gmail.com>
2013-05-21 09:26:13 +0200

    Refactoring

--- src/MailVerifier.java
+++ src/MailVerifier.java
@@ -1,9 +1,16 @@
 public class MailVerifier {
 
 	public boolean isValidEmailAddress(String email) {
+		return tieneSoloUnaArroba(email) && noTieneComasOEspacios(email);
+	}
+
+	private boolean noTieneComasOEspacios(String email) {
+		return !email.contains(" ") && !email.contains(",");
+	}
+
+	private boolean tieneSoloUnaArroba(String email) {
 		return email.contains("@")
-				&& email.indexOf("@") == email.lastIndexOf("@")
-				&& !email.contains(" ") && !email.contains(",");
+				&& email.indexOf("@") == email.lastIndexOf("@");
 	}
 
 }
