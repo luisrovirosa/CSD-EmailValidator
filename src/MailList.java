47a5dc6c6946c03d29f44d3da242bb4682d5df74
Luis <luisrovirosa@gmail.com>
2013-05-21 10:26:06 +0200

    Refactoring

--- src/MailList.java
+++ src/MailList.java
@@ -7,17 +7,25 @@ public class MailList {
 		ArrayList<String> lista = new ArrayList<String>();
 		if (emails.length() > 0) {
 			MailVerifier verifier = new MailVerifier();
-			String[] listaemails = emails.split(",");
+			String[] listaemails = splitEmails(emails);
 			for (String email : listaemails) {
-				String cleanedEmail = email.trim();
-				if (verifier.isValidEmailAddress(cleanedEmail)) {
-					lista.add(cleanedEmail);
+				if (verifier.isValidEmailAddress(email)) {
+					lista.add(email);
 				}
 			}
 
 		}
 		return lista;
 
+	}
+
+	private String[] splitEmails(String emails) {
+		String[] listaemails = emails.split(",");
+		String[] cleanedEmails = new String[listaemails.length];
+		for (int i = 0; i < listaemails.length; i++) {
+			cleanedEmails[i] = listaemails[i].trim();
+		}
+		return cleanedEmails;
 	}
 
 }
